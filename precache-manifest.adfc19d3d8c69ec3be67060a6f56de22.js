04da2394e7ea0f7c588b5f6bb4fddc1d0321a927
Oluwakayode <fadojuk@gmail.com>
2020-01-13 16:45:10 +0100

    Updates

---
+++ precache-manifest.adfc19d3d8c69ec3be67060a6f56de22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0456fe027321163be96f909fd804c54",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "fd218405b56eebc55f24",
+    "url": "/video-app/static/css/main.ca161dc0.chunk.css"
+  },
+  {
+    "revision": "3fb47ace0f344179e76e",
+    "url": "/video-app/static/js/2.cbd87861.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/video-app/static/js/2.cbd87861.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd218405b56eebc55f24",
+    "url": "/video-app/static/js/main.d8eb1684.chunk.js"
+  },
+  {
+    "revision": "42033dd4b7817da260be",
+    "url": "/video-app/static/js/runtime-main.66d6b95c.js"
+  }
+]);
